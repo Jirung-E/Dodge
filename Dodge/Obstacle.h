aba335f95657324e7bb1e79965577ef686160bd9
Jirung-E <jh10590326@gmail.com>
2021-01-18 17:55:47 +0900

    Add 'randomMoveSpeed()'

--- Dodge/Obstacle.h
+++ Dodge/Obstacle.h
@@ -2,11 +2,19 @@
 
 #include "GameObject.h"
 
+#include <chrono>
+
+
 class Obstacle : public GameObject {
 	const char obstacle_shape = '*';
+	int move_speed;	// n character / 1 second
+	std::chrono::system_clock::time_point last_move;
 
 public:
 	Obstacle();
 
 	void randomSpawn();
+	void randomMoveSpeed();
+
+	void Move();
 };
