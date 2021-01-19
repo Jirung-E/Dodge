0136e83484326b273d616e2417eb3c0e4bd61cdb
Jirung-E <jh10590326@gmail.com>
2021-01-19 16:39:09 +0900

    Add GameObject* world[width][height]

---
+++ Dodge/World.cpp
@@ -0,0 +1,4 @@
+#include "World.h"
+
+
+GameObject* world[World::Width][World::Height];
--- Dodge/World.h
+++ Dodge/World.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "GameObject.h"
+
+
 namespace World {
 	const int Width = 64;
 	const int Height = 16;
@@ -8,4 +11,6 @@ namespace World {
 	const int Min_y = 0;
 	const int Max_x = Min_x + Width - 1;
 	const int Max_y = Min_y + Height - 1;
+
+	extern GameObject* world[World::Width][World::Height];
 }
